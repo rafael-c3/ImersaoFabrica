391a4cc5d7e5dae7a4da83ec8eb1ae5d2d66085f
rafael-c3 <rafaellucas.c310@gmail.com>
2022-09-15 17:57:12 -0300

    Subindo projeto

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def chiclete(request):
+    return render(request, template_name='blog/home.html')
+
+def jujuba(request):
+    html = "<html><body><h1>Tome jujuba!</h1></body></body></html>"
+    return HttpResponse(html)
